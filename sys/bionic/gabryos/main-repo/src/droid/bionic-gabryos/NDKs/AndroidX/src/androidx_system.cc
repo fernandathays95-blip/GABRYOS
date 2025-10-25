bcde2b863f0e22d927107cddd16742244b4569f0
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-25 16:32:12 -0300

    Create androidx_system.cc

---
+++ sys/bionic/gabryos/main-repo/src/droid/bionic-gabryos/NDKs/AndroidX/src/androidx_system.cc
@@ -0,0 +1,10 @@
+#include "androidx_system.h"
+#include <iostream>
+
+namespace androidx {
+
+void checkNDK() {
+    std::cout << "[AndroidX/System] NDK AndroidX do GABRYOS carregado com sucesso." << std::endl;
+}
+
+}
