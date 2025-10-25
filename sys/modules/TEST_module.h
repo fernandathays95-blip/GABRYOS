d03f7381878fdb06548875ba436e5b3b82c22d64
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-25 17:40:38 -0300

    Create TEST_module.h

---
+++ sys/modules/TEST_module.h
@@ -0,0 +1,9 @@
+#ifndef TEST_MODULE_H
+#define TEST_MODULE_H
+
+#include <stdint.h>
+
+const uint8_t TEST_MODULE_DATA[] = {0,1,2,3,4,5,6,7,8,9};
+const size_t TEST_MODULE_SIZE = sizeof(TEST_MODULE_DATA);
+
+#endif
