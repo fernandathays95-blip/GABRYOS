f720757be67788a4bcc0e569c49bcdd4c0a59aa3
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-25 16:57:39 -0300

    Create kernl_cpu.h

---
+++ sys/bionic/gabryos/main-repo/src/droid/bionic-gabryos/kernel/kernl_cpu.h
@@ -0,0 +1,15 @@
+#ifndef KERNL_CPU_H
+#define KERNL_CPU_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void gabryos_init_cpu();
+void gabryos_log_cpu_info();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // KERNL_CPU_H
