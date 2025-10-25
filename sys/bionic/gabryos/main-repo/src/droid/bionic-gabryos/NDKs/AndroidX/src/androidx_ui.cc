f9ecf62390ab286ae9518790c21ddd470d2db929
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-25 16:31:00 -0300

    Create androidx_ui.cc

---
+++ sys/bionic/gabryos/main-repo/src/droid/bionic-gabryos/NDKs/AndroidX/src/androidx_ui.cc
@@ -0,0 +1,10 @@
+#include "androidx_ui.h"
+#include <iostream>
+
+namespace androidx {
+
+void renderWindow(const std::string &title) {
+    std::cout << "[AndroidX/UI] Renderizando janela: " << title << std::endl;
+}
+
+}
