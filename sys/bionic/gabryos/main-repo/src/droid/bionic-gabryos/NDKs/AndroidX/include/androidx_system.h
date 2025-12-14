9c630bf70abde9e63795f24094ed26d7f1ee590d
fernandathays95-blip <fernandathays95@gmail.com>
2025-12-14 18:30:54 -0300

    Delete sys directory

--- sys/bionic/gabryos/main-repo/src/droid/bionic-gabryos/NDKs/AndroidX/include/androidx_system.h
+++
@@ -1,8 +0,0 @@
-#ifndef ANDROIDX_SYSTEM_H
-#define ANDROIDX_SYSTEM_H
-
-namespace androidx {
-    void checkNDK();
-}
-
-#endif // ANDROIDX_SYSTEM_H
--- sys/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p build
-cd build
-cmake ..
-make
-echo "=============================="
-echo "GABRYOS Kernel compilado com sucesso!"
-echo "Executável: build/gabryos_kernel"
-echo "=============================="
